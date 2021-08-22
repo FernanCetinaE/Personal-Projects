263b674da59ed319b8fca4e6a1cc6ddf0f49c6b3
Fernan Cetina <73235964+FernanCetinaE@users.noreply.github.com>
2021-08-21 23:02:56 -0500

    Add files via upload
    Comentarios agregados a: Lista ligada para añadir horario por día de una asignatura

--- linkedlistsTry.cpp
+++ linkedlistsTry.cpp
@@ -15,21 +15,21 @@ void leer(int count);
 void imprimir();
 
 void leer(int count){
-    struct Node *newNode, *temp;
+    struct Node *newNode, *temp; //punteros de apoyo
 
-    head = (struct Node*)malloc(sizeof(struct Node)); 
+    head = (struct Node*)malloc(sizeof(struct Node)); //crea el primer nodo del struct
 
-    if(head == NULL)
+    if(head == NULL) //mensaje estandar de error al crear nodo
     {
         printf("Error para alojar memoria");
     }
     else
     {
         int diaHolder;
-        float entradaHolder, salidaHolder;
+        float entradaHolder, salidaHolder; //variables de asignacion
         //llenamos el primer nodo
         printf("Nombre: ");
-        gets(head -> asignatura);
+        gets(head -> asignatura); //para llenar "char" es necesario "gets"
 
         printf("\nDia de la semana: ");
         scanf("%d", &diaHolder);
@@ -43,12 +43,12 @@ void leer(int count){
         scanf("%f", &salidaHolder);
         head -> horaSalida = salidaHolder;
 
-        head->next = NULL; // Link the address field to NULL
-        temp = head;
+        head->next = NULL; // Link the address field to NULL, trunca primer nodo
+        temp = head;//puntero que apunta al primer nodo creado
 
         //Añadimos nodos siguientes
         for (int i = 2; i <= count; i++){
-        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode = (struct Node*)malloc(sizeof(struct Node));//creamos el espacio para el nuevo nodo
         
         /* If memory is not allocated for newNode */
         if(newNode == NULL)
@@ -59,7 +59,7 @@ void leer(int count){
         else
         {
 
-        strcpy(newNode -> asignatura, "12");
+        strcpy(newNode -> asignatura, "12"); //copiamos un "vacio" como referencia a "char"
         printf("\nDia de la semana: ");
         scanf("%d", &diaHolder);
         newNode -> diaSemana = diaHolder;
@@ -74,23 +74,26 @@ void leer(int count){
 
             newNode->next = NULL; // Link the address field of newNode with NULL
             temp->next = newNode; // Link previous node i.e. temp to the newNode
-            temp = temp->next; 
+            /*temp tenía guardado el nodo anterior y usará "next" apuntando a
+            newNode para enlazarlos
+            */
+            temp = temp->next; //se posiciona en el nuevo nodo creado
         }
     }
     }
 } 
 
 void imprimir(){
-    struct Node *pointer;
+    struct Node *pointer; //apuntador auxiliar
     if(head==NULL){
         printf("Lista vacia");
     }else{
-        pointer = head;
+        pointer = head; //se apunta el primer nodo del struct
 
         while(pointer != NULL){
             if(strlen(pointer->asignatura)>2){
             printf("\nMateria: %s", pointer->asignatura);
-            }
+            }//valor de refencia, para no repetir asignatura en cada nodo; evita redundancia
 
             printf("\nDia de la semana: %d", pointer -> diaSemana);
             printf("\nEntrada: %.2f", pointer -> horaEntrada);
@@ -102,7 +105,7 @@ void imprimir(){
 
 int main(){
     
-    leer(2);
+    leer(2); //valor editable de cantidad de días por agregar, por simplicidad 
     imprimir();
     return 0;
 }
