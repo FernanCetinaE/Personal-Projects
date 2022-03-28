4a6a53662612a9d0d8642a7dcb8db07f90031320
Fernan Cetina <73235964+FernanCetinaE@users.noreply.github.com>
2022-03-28 09:49:30 -0600

    Sample of code snippet that speeds up code
    There is a declaration that helps on run time to lower time of execution

---
+++ SpeedCodeUp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define mas ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+int main()
+{
+    mas;
+    for (int i = 0; i < 1000; i++)
+    {
+        for (int j = 1; j < 1500; j++)
+        {
+            /* code */
+        }
+        
+    }
+
+    return 0;
+}
